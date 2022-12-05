f58c6b01cd9609d28cd0f9238b83c435501322b0
Shridhara Chaube <chaubey.shridhara@gmail.com>
2022-12-05 16:20:26 +0530

    Shri

---
+++ SampleRest_6/SampleRest_6/src/main/java/com/example/SampleRest_6/SampleRest6Application.java
@@ -0,0 +1,13 @@
+package com.example.SampleRest_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRest6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRest6Application.class, args);
+	}
+
+}
